09fd199624ea12be8bc530db56ef022fd8a76c6a
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2025-02-10 22:11:07 -0600

    Flipping a fair coin

---
+++ 1.ElementsOfProgramming/1.3ConditionalsAndLoops/Flip.java
@@ -0,0 +1,8 @@
+public class Flip
+{
+ public static void main(String[] args)
+ { // Simulate a fair coin flip.
+ if (Math.random() < 0.5) System.out.println("Heads");
+ else System.out.println("Tails");
+ }
+}
