54df332b56147e291ff0e55c061779971fe6e0a5
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2025-02-11 18:23:56 -0600

    Newtons method, we can configure the precison of finding the Square root by changing the Epsilon number value

--- 1.ElementsOfProgramming/1.3ConditionalsAndLoops/1.3.6/Sqrt.java
+++ 1.ElementsOfProgramming/1.3ConditionalsAndLoops/1.3.6/Sqrt.java
@@ -4,11 +4,11 @@ public class Sqrt
  {
   // c given is 2
  double c = Double.parseDouble(args[0]);
- double EPSILON = 1e-15; // 0.000000000000001
+ double EPSILON = 1e-5; // 0.000000000000001
  double t = c; // t = 2
-  // 2 - 2/2 = 1 > 0.000000000000001 * 2 = 0.000000000000002
+  
  while (Math.abs(t - c/t) > EPSILON * t)
- { // Replace t by the average of t and c/t.
+ { 
  t = (c/t + t) / 2.0;
  }
  System.out.println(t);
